1c283d28f3f803de3b214c3f9c6ca9e3a35fe042
Yashaswini2004 <yashaswinic31@gmail.com>
2025-11-19 13:44:38 +0530

    first code

--- class1/src/main.jsx
+++ class1/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import './index.css'
-=======
 
->>>>>>> 610e74f (first code)
+import './index.css'
+
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
--- class1/src/pages/Page5.jsx
+++ class1/src/pages/Page5.jsx
@@ -1,6 +1,36 @@
 import React,{Component} from "react";
 let x=12;
 const y=25.66;
+const colors=["red","blue","pink"];
+let users=[
+    {
+        id:1,
+        name:"john",
+        email:"john@gmail.com",
+        age:90,
+        address:{
+            city:"bangalore"
+        }
+    },
+     {
+        id:2,
+        name:"yashu",
+        email:"yashaswini@gmail.com",
+        age:21,
+        address:{
+            city:"tumkur"
+        }
+    },
+     {
+        id:3,
+        name:"pavitra",
+        email:"pavitra@gmail.com",
+        age:22,
+        address:{
+            city:"mangalore"
+        }
+    },
+]
 class Page5 extends Component{
     constructor(props){
         super(props)
@@ -22,6 +52,27 @@ class Page5 extends Component{
                 <hr />
                 <h4>p={this.p}</h4>
                 <h4>q={this.q}</h4>
+                <hr/>
+                <div>
+                    {colors?.map(function(item,index){
+                        return(
+                            <p key={index}>{item}</p>
+                        )
+                    })}
+                </div>
+                <hr />
+                <div>
+                    {users.map((user,index)=>{
+                        return (
+                            <div key={user.id}>
+                                <h2>Name={user.name}</h2>
+                                <p>email={user.email}</p>
+                                <p>age={user.age}</p>
+                                <p>city={user.address.city}</p>
+                            </div>
+                        )
+                    })}
+                </div>
              </div>
  
         )
