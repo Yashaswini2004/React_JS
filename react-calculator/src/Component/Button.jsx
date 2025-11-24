cf3886cd26daad7405792c6c65903ef157fb4a55
Yashaswini2004 <yashaswinic31@gmail.com>
2025-11-24 13:34:58 +0530

    Third section

---
+++ react-calculator/src/Component/Button.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+class Button extends Component{
+   constructor(props){
+    super(props)
+   }
+   render(){
+    return (
+        <div className="btn" onClick={()=>this.props.btnHandler(this.props.val)}>
+          {this.props.val}
+        </div>
+    )
+   }
+}
+export default Button;
